c9ed028b7a97fae5150f022b706f701b2a802c41
Casey Childers <cachilders@gmail.com>
2017-11-07 06:12:14 -0800

    [feature] add interval

--- app.py
+++ app.py
@@ -1,15 +1,25 @@
-"""Let's broadcast to AWS"""
-import requests
 import json
 from uuid import getnode as get_mac
+import threading
+import requests
 
-MA = hex(get_mac())
-MAC = ':'.join([MA[i:i+2] for i in range(0, len(MA), 2)][1:])
+def set_interval(func, secs):
+  def enclose():
+    set_interval(func, secs)
+    func()
+  t = threading.Timer(secs, enclose)
+  t.start()
+  return t
+
+MAC = hex(get_mac())
+NAME = ':'.join([MAC[i:i+2] for i in range(0, len(MAC), 2)][1:])
 URL = 'http://tessellatecore-env.hfeqqqyqqv.us-east-1.elasticbeanstalk.com/io/tessellate'
-PAYLOAD = json.dumps({'name': MAC, 'message': 'Python Prealpha'})
+PAYLOAD = json.dumps({'name': NAME, 'message': 'Python Prealpha'})
 HEADERS = {'content-type': 'application/json; charset=utf-8', 'content-length': len(PAYLOAD)}
 
-req = requests.post(url=URL, headers=HEADERS, data = PAYLOAD)
-res = req.text
+def broadcast():
+  req = requests.post(url=URL, headers=HEADERS, data = PAYLOAD)
+  res = req.text
+  print(res)
 
-print(res)
+interval = set_interval(broadcast, 60)
