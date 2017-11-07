c8e77dd80916df3e6f17d066501536741919243a
Casey Childers <cachilders@gmail.com>
2017-11-06 21:24:22 -0800

    [tweak] add condition check to avoid multiple intervals

--- post-data.lua
+++ post-data.lua
@@ -3,23 +3,25 @@ local payload = {
   ['message'] = 'NodeMCU Lua Prealpha'
 }
 
-print('Sending', sjson.encode(payload))
+print('Broadcasting', sjson.encode(payload))
 
-timer=tmr.create()
+if not timer then
+  timer=tmr.create()
 
-timer:register(60000, tmr.ALARM_AUTO, function()
-  http.post(
-    'http://tessellatecore-env.hfeqqqyqqv.us-east-1.elasticbeanstalk.com/io/tessellate',
-    'accept: application/json\r\n'..'content-type: application/json\r\n'..'content-length: '..#sjson.encode(payload)..'\r\n',
-    sjson.encode(payload),
-    function(code, data)
-      if (code < 0) then
-        print('HTTP request failed')
-      else
-        print(code, data)
+  timer:register(60000, tmr.ALARM_AUTO, function()
+    http.post(
+      'http://tessellatecore-env.hfeqqqyqqv.us-east-1.elasticbeanstalk.com/io/tessellate',
+      'accept: application/json\r\n'..'content-type: application/json\r\n'..'content-length: '..#sjson.encode(payload)..'\r\n',
+      sjson.encode(payload),
+      function(code, data)
+        if (code < 0) then
+          print('HTTP request failed')
+        else
+          print(code, data)
+        end
       end
-    end
-  )
-end)
+    )
+  end)
 
-timer:start()
+  timer:start()
+end
