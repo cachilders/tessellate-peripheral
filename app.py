c6cf7288c9e85b1b7db6c27e25ad1f5513247cac
Casey Childers <cachilders@gmail.com>
2017-11-07 19:49:35 -0800

    [bug out] resolve type error and trim mac more accurately

--- app.py
+++ app.py
@@ -12,10 +12,10 @@ def set_interval(func, secs):
   return t
 
 MAC = hex(get_mac())
-NAME = ':'.join([MAC[i:i+2] for i in range(0, len(MAC), 2)][1:])
+NAME = ':'.join([MAC[i:i+2] for i in range(0, len(MAC), 2)][1:7])
 URL = 'http://tessellatecore-env.hfeqqqyqqv.us-east-1.elasticbeanstalk.com/io/tessellate'
 PAYLOAD = json.dumps({'name': NAME, 'message': 'Python Prealpha'})
-HEADERS = {'content-type': 'application/json; charset=utf-8', 'content-length': len(PAYLOAD)}
+HEADERS = {'content-type': 'application/json; charset=utf-8', 'content-length': str(len(PAYLOAD))}
 
 def broadcast():
   req = requests.post(url=URL, headers=HEADERS, data = PAYLOAD)
