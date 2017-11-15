53fd975b56a7ae95a8be856f860b7eeb7be39ecb
Casey Childers <cachilders@gmail.com>
2017-11-14 22:31:09 -0800

    [cleanup] formatting

--- tessellate.ino
+++ tessellate.ino
@@ -28,13 +28,13 @@ void loop() {
   
   byte mac[6];
   WiFi.macAddress(mac);
-  String name = String(mac[5],HEX) + ":" + 
-    String(mac[4],HEX) + ":" + 
-    String(mac[3],HEX) + ":" +
-    String(mac[2],HEX) + ":" +
-    String(mac[1],HEX) + ":" +
-    String(mac[0],HEX);
-  String message = "NodeMCU Arduino Prealpha";
+  String name = String(mac[5], HEX) + ":" + 
+                String(mac[4], HEX) + ":" + 
+                String(mac[3], HEX) + ":" +
+                String(mac[2], HEX) + ":" +
+                String(mac[1], HEX) + ":" +
+                String(mac[0], HEX);
+  String message = "ESP8266 Arduino Prealpha";
   String url = "/io/tessellate";
   String payload = "{\"name\":\" " + name + "\", \"message\":\" " + message + "\"}";
   
