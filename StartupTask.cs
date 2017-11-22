c1237cec465657fb91e7898aa3cfbea0b9888718
Casey Childers <cachilders@gmail.com>
2017-11-22 00:40:01 -0800

    [wip] build initial post request

--- StartupTask.cs
+++ StartupTask.cs
@@ -1,8 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Collections.Specialized;
 using System.Text;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using Windows.ApplicationModel.Background;
 
 // The Background Application template is documented at http://go.microsoft.com/fwlink/?LinkID=533884&clcid=0x409
@@ -11,15 +11,28 @@ namespace TessellatePeripheral
 {
     public sealed class StartupTask : IBackgroundTask
     {
-        public void Run(IBackgroundTaskInstance taskInstance)
+        public async void Run(IBackgroundTaskInstance taskInstance)
         {
-            // 
-            // TODO: Insert code to perform background work
-            //
-            // If you start any asynchronous methods here, prevent the task
-            // from closing prematurely by using BackgroundTaskDeferral as
-            // described in http://aka.ms/backgroundtaskdeferral
-            //
+            BackgroundTaskDeferral deferral = taskInstance.GetDeferral();
+
+            var uri = new Uri("http://tessellatecore-env.hfeqqqyqqv.us-east-1.elasticbeanstalk.com/io/tessellate");
+
+            // string name = "00:00:11:00:00";
+            // string message = "Dragonboard Windows IoT Prealpha";
+
+            string payload = "{\"name\": \"00:00:00:00:00\", \"message\": \"Dragonboard Windows IoT Prealpha\"}";
+
+            var client = new HttpClient();
+            var request = new HttpRequestMessage(HttpMethod.Post, uri);
+            request.Content = new StringContent(payload, Encoding.UTF8, "application/json");
+
+            System.Diagnostics.Debug.WriteLine(request);
+
+            var response = await client.SendAsync(request);
+
+            System.Diagnostics.Debug.WriteLine(response);
+
+            deferral.Complete();
         }
     }
 }
