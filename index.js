144d1dd406f945704edfabcd807a8c201f53ada2
Casey Childers <cachilders@gmail.com>
2017-12-03 23:39:44 -0800

    [init] poc scans for bean uuids and makes posts if they're in range

--- index.js
+++ index.js
@@ -1,11 +1,47 @@
+var bodyParser = require('body-parser');
 var express = require('express');
 var app = express();
-var bodyParser = require('body-parser');
+var noble = require('noble');
 var os = require('os');
 var reduce = require('lodash.reduce');
 var request = require('request');
 
 var postData = require('./controllers/post-data');
+
+var RSSI_THRESHOLD = -90;
+var EXIT_GRACE_PERIOD = 20000;
+var WHITELIST = ['A495']; // <-- Bean UUID prefix;  ALL-CAPS
+var inRange = {};
+
+noble.on('stateChange', function(state) {
+  if (state === 'poweredOn') {
+    noble.startScanning([], true);
+  } else {
+    noble.stopScanning();
+  }
+});
+
+noble.on('discover', function(peripheral) {
+  var services = peripheral.advertisement.serviceUuids;
+  var id = peripheral.id;
+  var entered = !inRange[id];
+  if (services.length > 0 && WHITELIST.includes(services[0].slice(0, 4).toUpperCase())) {
+    if (entered) {
+      inRange[id] = {
+        peripheral: peripheral
+      };
+    }
+    inRange[id].lastSeen = Date.now();
+  }
+});
+
+setInterval(function() {
+  for (var id in inRange) {
+    if (inRange[id].lastSeen < (Date.now() - EXIT_GRACE_PERIOD)) {
+      delete inRange[id];
+    }
+  }
+}, EXIT_GRACE_PERIOD / 2);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -13,29 +49,23 @@ app.use(bodyParser.urlencoded({
 }));
 app.use('/tessellate', postData);
 
-var name = reduce(os.networkInterfaces(), (mac, net) => {
-  if (!mac && Array.isArray(net) && net[0].mac) {
-    if (reduce(net[0].mac.split(/:/), (sum, num) => {
-      return sum + parseInt(num, 16)
-    }, 0) > 0) {
-      mac = net[0].mac;
-    }
-  }
-  return mac;
-}, '');
-
-var payload = {
-  name,
-  message: 'Device-Agnostic JS Prealpha',
-};
 
 setInterval(() => {
-  var httpRequestOptions = {
-    url: 'http://localhost:1976/tessellate',
-    form: payload
-  };
-  console.log('Sending:', payload)
-  request.post(httpRequestOptions, function(error, response, body){});
+  for (var id in inRange) {
+    var peripheral = inRange[id].peripheral;
+    var name = id.split(':').length === 1 ? id.match(/.{2}/g).join(':') : id;
+    var message = peripheral.advertisement.localName;
+    var payload = {
+      name,
+      message: message + ' via RPi3 JS BLE Gateway Prealpha',
+    };
+    var httpRequestOptions = {
+      url: 'http://localhost:1976/tessellate',
+      form: payload
+    };
+    console.log('Sending:', payload)
+    request.post(httpRequestOptions, function(error, response, body){});
+  }
 }, 60000);
 
 app.set('port', 1976);
